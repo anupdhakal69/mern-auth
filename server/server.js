4041864c3c813a7448f1e7cd1c30bd3f72f38339
Anup D <dhakalanup96@gmail.com>
2024-12-08 12:09:14 +0545

    database connection done

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 import cookieParser from "cookie-parser"
+import connectDB from "./config/db.js"
 
 const app = express()
 const port = process.env.PORT || 4000
 
+connectDB()
 app.use(cors({credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
@@ -13,7 +15,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.send("Hello World")
-    })
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
