ab02e9c82a86819bec9f84259ef3571b4b56aa41
Anup D <dhakalanup96@gmail.com>
2024-12-13 08:36:31 +0545

    api routes done

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import "dotenv/config"
 import cookieParser from "cookie-parser"
 import connectDB from "./config/db.js"
+import authRouter from "./routes/AuthRoutes.js"
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -13,9 +14,12 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 
+//API Endpoints
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
+
+app.use("/api/auth", authRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
