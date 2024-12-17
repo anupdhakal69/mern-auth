ea528a475d21b623cd320ec1cb15b9fa0f01d8df
Anup D <dhakalanup96@gmail.com>
2024-12-17 20:42:23 +0545

    middleware config

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { register,login, logout, getUser } from '../controller/AuthController.js'
+import { register,login, logout, getUser, sendVerifyOtp, verifyEmail } from '../controller/AuthController.js'
+import userAuth from '../middleware/userAuth.js'
 const authRouter = express.Router()
 
 authRouter.post('/register', register)
 authRouter.post('/login', login)
 authRouter.post('/logout', logout)
 authRouter.get('/users', getUser)
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp)
+authRouter.post('/verify-account', userAuth, verifyEmail)
 
 export default authRouter
